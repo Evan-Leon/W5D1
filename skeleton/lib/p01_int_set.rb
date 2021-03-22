451f1f677506dc0f1974975b9806b48659879eba
Evan-Leon <evan.leon.j@gmail.com>
2021-03-22 11:52:31 -0400

    Defined initialize, insert and include

---
+++ skeleton/lib/p01_int_set.rb
@@ -0,0 +1,89 @@
+class MaxIntSet
+  attr_accessor :max, :store
+  def initialize(max)
+    @max = max
+    @store = Array.new(max)
+  end
+
+  def insert(num)
+    raise 'Out of bounds' if num > max || num < 0 
+    return true if @store[0] != nil  
+    if num < max 
+      @store[num+1] = true
+      return true
+    end
+  end
+
+  def remove(num)
+
+  end
+
+  def include?(num)
+    @store[num + 1]
+  end
+
+  private
+
+  def is_valid?(num)
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
