d300962f94cf0808cfd80ba451d1f377566e7beb
jaewooklim1 <jaewooklim1@gmail.com>
2021-03-22 12:12:59 -0400

    working on intset

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -5,21 +5,25 @@ class MaxIntSet
     @store = Array.new(max)
   end
 
+
+  # it "values in the store should be booleans corresponding to presence" do
+  #   set.insert(1)
+  #   expect(set.store[1]).to be true
   def insert(num)
     raise 'Out of bounds' if num > max || num < 0 
-    return true if @store[0] != nil  
+    # return true if @store[0] != nil  
     if num < max 
-      @store[num+1] = true
+      @store[num] = true
       return true
     end
   end
 
   def remove(num)
-
+    @store[num] = nil
   end
 
   def include?(num)
-    @store[num + 1]
+    @store[num]
   end
 
   private
@@ -30,20 +34,32 @@ class MaxIntSet
   def validate!(num)
   end
 end
-
+require "byebug"
 
 class IntSet
+
+
+  attr_accessor :num_buckets
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    # debugger
+    idx = num % num_buckets
+    @store[idx] << num
+    # debugger
   end
 
   def remove(num)
   end
 
   def include?(num)
+    # debugger
+    return false if @store[num % num_buckets].include?(num)
+    true
   end
 
   private
